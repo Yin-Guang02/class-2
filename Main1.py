dcb28617e45e7bab0aabf81e13c24b009cda4c23
Yin-Guang02 <deekshitha.f20@gmail.com>
2026-03-13 11:35:42 +0300

    13th march

---
+++ Main1.py
@@ -0,0 +1,21 @@
+name = "Deekshitha"
+age=15
+is_student=True
+weight=35.9
+
+print("Name of person:",name)
+print("data type of name is", type(name))
+print("Age:",age)
+print("data type of age is", type(age))
+print("Student:",is_student)
+print("data type of is_student is", type(is_student))
+print("weight of the person:",weight)
+print("data type of weight is", type(weight))
+
+print("\n after type casting...")
+age=str(age)
+print(age)
+print("data type of age:", type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is:",type(weight) )
